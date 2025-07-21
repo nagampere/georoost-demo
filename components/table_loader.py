32dba55cf9ab622472d2b965a21706d7ae66df98
nagampere <nagayohei0508@gmail.com>
2025-07-21 21:47:23 +0900

    テーブルスキーマ取得先の変更

--- components/table_loader.py
+++ components/table_loader.py
@@ -3,7 +3,8 @@ import streamlit as st
 # DuckDB からテーブル一覧を取得
 def get_tables(con):
     try:
-        tables = con.execute("select * from information_schema.tables").fetchdf()["table_name"].tolist()
+        tables = con.execute("select * from information_schema.tables where table_catalog = 'georoost-demo'").fetchdf()["table_name"].tolist()
+        tables.sort()
         return tables if tables else []
     except Exception as e:
         st.error(f"テーブル一覧の取得に失敗しました: {e}")
